061401752390d951ac9ab13c996d1924fb7c1a21
BenjaminRiderelli <riderelli.ben@gmail.com>
2023-10-22 21:50:52 +0200

    small css spellcheck

--- src/components/main/aside/index.jsx
+++ src/components/main/aside/index.jsx
@@ -4,7 +4,7 @@ import Pagination from "./pagination";
 
 const Aside = ({ setQuery, totalCount }) => {
   return (
-    <aside className="flex flex-col md:min-w-[20%] md:max-w-[20%] gap-4 mmin-h-[100vh] bg-white overflow-auto">
+    <aside className="flex flex-col md:min-w-[320px] md:max-w-[20%] gap-4 min-h-[100vh] bg-white overflow-auto">
       <Filter setQuery={setQuery} />
       <NavBar />
       <Pagination setQuery={setQuery} totalCount={totalCount} />
